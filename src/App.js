4c381383a5369252801162a00f9350447d8c38a7
Andres Betancourt <aebb001@gmail.com>
2021-01-15 15:16:43 -0400

    Configuracion de react con tailwind

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className="bg-yellow-500 text-white p-12 text-center text-2xl font-bold font-serif font-bonita">
+        Hola Tailwind
+      </h1>
+      <p className="font-bold text-center mt-6">
+        Configuracion inicial de tailwind con create-react-app
+      </p>
+
+      <div className="flex text-center justify-between mt-24 bg-yellow-500 p-12 ">
+        <div className="caja w-72  bg-blue-500">Box 1</div>
+        <div className="caja w-72 bg-blue-500">Box 2</div>
+        <div className="caja w-72 bg-blue-500">Box 2</div>
+      </div>
+      <button className="boton hover:nuevoboton">Soy un botton</button>
+      <button disabled class="boton disabled:opacity-25 bg-blue-500">
+        Submit
+      </button>
     </div>
   );
 }
